232f2d2f7daa8146289852fc321adb90cdf64ebf
xkevin190 <kevinvelasco190@gmail.com>
2019-04-04 16:46:33 -0400

    googleMap iconst complete

--- src/views/PersonalExterno/ModalExternals/Map.js
+++ src/views/PersonalExterno/ModalExternals/Map.js
@@ -7,28 +7,31 @@ import {
   Marker,
   InfoWindow
 } from "react-google-maps"
+import styled from 'styled-components'
 
 const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
 
   return (
     <GoogleMap defaultZoom={13} defaultCenter={{lat: 9.891661, lng: -67.354302 }}>
       {props.markers.map((marker, i ) => {
-        console.log("0", props.selectedMarker === marker , "1", props.selectedMarker ,"2", marker )
         const onClick = props.onClick.bind(this, marker)
         return (
           <Marker
             key={i}
-            icon={{url:'https://i.pinimg.com/originals/7f/9b/9a/7f9b9a39639b9ca8bb6f1cba6e35d041.png',
+            icon={{url:`${marker.logo}`,
             scaledSize:{ width: 20 , height: 20 }
           }}
             onClick={onClick}
             onMouseOver={onClick}
+            onMouseOut={props.deletePosition}
             position={{ lat: marker.lat, lng: marker.lng }}
           >
             {(props.selectedMarker.lat === marker.lat) && (props.selectedMarker.lng === marker.lng)&&
               <InfoWindow>
                 <div>
-                   as clicked here
+                  <h4 style={{borderBottom:'1px solid #eadfd1' }}>{marker.title}</h4>
+                   <br/>
+                  <div style={{paddingBottom:20}}>{marker.description}</div>
                 </div>
               </InfoWindow>
             }
@@ -40,4 +43,5 @@ const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
 })
 
 export default MapWithAMarker
+
 
--- src/views/PersonalExterno/ModalExternals/externalModal.js
+++ src/views/PersonalExterno/ModalExternals/externalModal.js
@@ -13,15 +13,20 @@ class ExternalModal extends React.Component {
     this.setState({selectedMarker: value})
   };
 
+  deletePosition = () =>{
+    console.log('execute')
+    this.setState({selectedMarker: false})
+  }
+
   render() {
     const { open, close } = this.props;
     const data = [
-      { lat: 9.910057492847013, lng: -67.36354452596436, logo:'https://i.pinimg.com/originals/7f/9b/9a/7f9b9a39639b9ca8bb6f1cba6e35d041.png'   },
-      { lat: 9.907373018724995, lng: -67.37723452077637, logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Coca-Cola_bottle_cap.svg/1200px-Coca-Cola_bottle_cap.svg.png' },
-      { lat: 9.923458397094366, lng: -67.34097105489502, logo:'https://www.rioshopping.com/-/media/images/b2c/spain/valladolid/logo-stores/logos-nuevos-redimensionados/movistar_410x282.png?h=282&la=es-ES&mw=650&w=410&hash=412B7DD5082105C7BFA7334CCA738A7DAA0D1B13' },
-      { lat: 9.89830482956145, lng: -67.35015493856201, logo:'https://pbs.twimg.com/profile_images/936720559957774342/w9pjw8nh_400x400.jpg' },
-      { lat: 9.926417513421839, lng: -67.3476658485962, logo:'https://media.licdn.com/dms/image/C4E0BAQHbbG0eOfaDJA/company-logo_200_200/0?e=2159024400&v=beta&t=LmRMbEZO-0DlOGTCQm5br9xN_TFDVK2rsc8vv_TLmck' },
-      { lat: 9.923469352924165, lng: -67.37631105408468, logo:'https://pbs.twimg.com/ext_tw_video_thumb/1067239661901205510/pu/img/41oJ_QPgGoshk2du.jpg' }
+      {title:'tecnocam', description:'esta son las caracteristicas de tecnocam', lat: 9.910057492847013, lng: -67.36354452596436, logo:'https://i.pinimg.com/originals/7f/9b/9a/7f9b9a39639b9ca8bb6f1cba6e35d041.png' },
+      {title:'tecnocam', description:'esta son las caracteristicas de tecnocam', lat: 9.907373018724995, lng: -67.37723452077637, logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Coca-Cola_bottle_cap.svg/1200px-Coca-Cola_bottle_cap.svg.png' },
+      {title:'tecnocam', description:'esta son las caracteristicas de tecnocam', lat: 9.923458397094366, lng: -67.34097105489502, logo:'https://www.rioshopping.com/-/media/images/b2c/spain/valladolid/logo-stores/logos-nuevos-redimensionados/movistar_410x282.png?h=282&la=es-ES&mw=650&w=410&hash=412B7DD5082105C7BFA7334CCA738A7DAA0D1B13' },
+      {title:'tecnocam', description:'esta son las caracteristicas de tecnocam', lat: 9.89830482956145, lng: -67.35015493856201, logo:'https://pbs.twimg.com/profile_images/936720559957774342/w9pjw8nh_400x400.jpg' },
+      {title:'tecnocam', description:'esta son las caracteristicas de tecnocam', lat: 9.926417513421839, lng: -67.3476658485962, logo:'https://media.licdn.com/dms/image/C4E0BAQHbbG0eOfaDJA/company-logo_200_200/0?e=2159024400&v=beta&t=LmRMbEZO-0DlOGTCQm5br9xN_TFDVK2rsc8vv_TLmck' },
+      {title:'tecnocam', description:'esta son las caracteristicas de tecnocam', lat: 9.923469352924165, lng: -67.37631105408468, logo:'https://i.pinimg.com/originals/7f/9b/9a/7f9b9a39639b9ca8bb6f1cba6e35d041.png' }
     ];
     return (
       <Modal isOpen={open} toggle={close} style={{ minWidth: "65%" }}>
@@ -31,6 +36,7 @@ class ExternalModal extends React.Component {
             selectedMarker={this.state.selectedMarker}
             markers={data}
             onClick={this.handleClick}
+            deletePosition={this.deletePosition}
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwl7QwHKe7NFx28t-CbMDTUdQMFVrjEz4&callback=initMap"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
