706fa65312094c0f146486cd151ef1ced87ba573
WilliamRa <wiliamra.16@gmail.com>
2019-06-27 16:50:28 -0400

    Agregar Paginado (ListDistributor)

---
+++ src/components/Pagination.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { TablePagination, TableFooter, TableRow, IconButton } from '@material-ui/core'
+import { LastPage, FirstPage, KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const actionsStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5,
+  },
+});
+
+class TablePaginationActions extends Component {
+  handleFirstPageButtonClick = event => {
+    this.props.onChangePage(event, 0);
+  };
+
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  handleLastPageButtonClick = event => {
+    this.props.onChangePage(
+      event,
+      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1),
+    );
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+    console.log("aqui",count);
+
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="First Page">
+          {theme.direction === 'rtl' ? <LastPage /> : <FirstPage />}
+        </IconButton>
+
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page">
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page">
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+
+        <IconButton
+          onClick={this.handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Last Page">
+          {theme.direction === 'rtl' ? <FirstPage /> : <LastPage />}
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
+  TablePaginationActions,
+);
+
+class Pagination extends Component {
+  render() {
+    console.log(this.props.contador.length);
+    return (
+      <TableFooter>
+        <TableRow>
+          <TablePagination
+            count={this.props.contador.length}
+            page={this.props.page}
+            rowsPerPageOptions={[5, 20, 50]}
+            rowsPerPage={this.props.rowsPerPage}
+            onChangeRowsPerPage={this.props.handleChangeRowsPerPage}
+            onChangePage={this.props.handleChangePage}
+            ActionsComponent={TablePaginationActionsWrapped}
+            labelRowsPerPage={ ['Filas por página:'] }
+          >
+          </TablePagination>
+        </TableRow>
+      </TableFooter>
+    );
+  }
+}
+
+export default Pagination;
--- src/views/Distributor/ListDistributor.js
+++ src/views/Distributor/ListDistributor.js
@@ -4,60 +4,63 @@ import IconButton from "@material-ui/core/IconButton";
 import { Delete, Edit, Visibility } from "@material-ui/icons";
 import { GetDisabledPermits } from "../../core/utils";
 import ModalDistributor from './ModalDistributor.js';
+import Pagination from '../../components/Pagination';
 
 class ListDistributor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal:false,
+      modal: false,
       modalHeader: '',
       modalFooter: '',
       action: '',
       disabled: '',
       showHide: '',
-      option:0,
-      position: 0,  
-      userId: '',      
-    };    
+      option: 0,
+      position: 0,
+      userId: '',
+      page: 0,
+      rowsPerPage: 5,
+      cont: 0
+    };
   }
 
-
-  openModal = (option, pos, id) => {  
-    if(option === 1){
+  openModal = (option, pos, id) => {
+    if (option === 1) {
       this.setState({
-        modal:true,
-        option:option,
-        modalHeader:'Registrar Proveedor',
-        modalFooter:'Guardar',
-        disabled: false,
-        showHide: 'show',         
-      })
-    }else if(option === 2){
-      this.props.LoadDistributorIdFunction(id);
-      this.setState({
-        modal:true,
-        option:option,
-        modalHeader:'Ver Proveedor',
-        modalFooter:'Guardar',
-        disabled: true,
-        showHide: 'hide',                
-      })
-    }else if(option === 3){
-      this.props.LoadDistributorIdFunction(id);
-      this.setState({
-        modal:true,
-        option:option,
-        modalHeader:'Editar Proveedor',
-        modalFooter:'Editar',
+        modal: true,
+        option: option,
+        modalHeader: 'Registrar Proveedor',
+        modalFooter: 'Guardar',
         disabled: false,
         showHide: 'show',
-        position: pos,        
-        userId:id       
       })
-    }  
-  }  
+    } else if (option === 2) {
+      this.props.LoadDistributorIdFunction(id);
+      this.setState({
+        modal: true,
+        option: option,
+        modalHeader: 'Ver Proveedor',
+        modalFooter: 'Guardar',
+        disabled: true,
+        showHide: 'hide',
+      })
+    } else if (option === 3) {
+      this.props.LoadDistributorIdFunction(id);
+      this.setState({
+        modal: true,
+        option: option,
+        modalHeader: 'Editar Proveedor',
+        modalFooter: 'Editar',
+        disabled: false,
+        showHide: 'show',
+        position: pos,
+        userId: id
+      })
+    }
+  }
 
-  deleteProveedor = (proveedorId) => {  
+  deleteProveedor = (proveedorId) => {
     const message = {
       title: "Eliminar Proveedor",
       info: "¿Esta seguro que desea eliminar este Proveedor?"
@@ -66,73 +69,108 @@ class ListDistributor extends React.Component {
       if (res) {
         this.props.DeleteDistributorAction(proveedorId);
       }
-    });    
-  } 
+    });
+  }
 
-  valorCloseModal = (valor) => {            
+  valorCloseModal = (valor) => {
     this.setState({
-        modal: valor,          
-        option: 0,  
-    });                    
-  } 
+      modal: valor,
+      option: 0,
+    });
+  }
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ page: 0, rowsPerPage: event.target.value });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
 
   render() {
-      const updateDisabled = GetDisabledPermits(this.props.distributorPermits, "Update");
-      const deleteDisabled = GetDisabledPermits(this.props.distributorPermits, "Delete");
-      const createDisabled = GetDisabledPermits(this.props.distributorPermits, "Create");
-    
+    const updateDisabled = GetDisabledPermits(this.props.distributorPermits, "Update");
+    const deleteDisabled = GetDisabledPermits(this.props.distributorPermits, "Delete");
+    const createDisabled = GetDisabledPermits(this.props.distributorPermits, "Create");
+    const { rowsPerPage, page } = this.state;
+    const ArrayDistributor = [];
+
+    this.props.listDistributor.map((distributor, key) => {
+      ArrayDistributor.push({
+        ...distributor, number: key + 1
+      })
+    })
+
     return (
       <div>
-        <ModalDistributor 
-          option = {this.state.option}
-          modal = {this.state.modal}
-          modalHeader = {this.state.modalHeader}
-          modalFooter = {this.state.modalFooter}
-          disabled = {this.state.disabled}
-          showHide = {this.state.showHide}   
-          userId = {this.state.userId}        
-                       
-          valorCloseModal = {this.valorCloseModal}          
+        <ModalDistributor
+          option={this.state.option}
+          modal={this.state.modal}
+          modalHeader={this.state.modalHeader}
+          modalFooter={this.state.modalFooter}
+          disabled={this.state.disabled}
+          showHide={this.state.showHide}
+          userId={this.state.userId}
+          valorCloseModal={this.valorCloseModal}
         />
         <Button color="success" disabled={createDisabled} onClick={() => { this.openModal(1); }}>Agregar Proveedor</Button>
         <br />
-        <br />        
-          <Table hover responsive borderless>
-            <thead className="thead-light">
-              <tr>
-                <th className="text-left">Nro</th>
-                <th className="text-left" style={{'minWidth':"105px"}}>DNI</th>
-                <th className="text-left">Proveedor</th>
-                <th className="text-left">Email</th>
-                <th className="text-left">Telefono</th>                                    
-                <th className="text-left" style={{'minWidth':"205px"}}>Acciones</th>                  
-              </tr>
-            </thead>
-            <tbody>
-             {this.props.listDistributor? this.props.listDistributor.map((distributor, i) => {
+        <br />
+        <Table hover responsive borderless>
+          <thead className="thead-light">
+            <tr>
+              <th className="text-left">Nro</th>
+              <th className="text-left" style={{ 'minWidth': "105px" }}>DNI</th>
+              <th className="text-left">Proveedor</th>
+              <th className="text-left">Email</th>
+              <th className="text-left">Telefono</th>
+              <th className="text-left" style={{ 'minWidth': "205px" }}>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ArrayDistributor ? ArrayDistributor.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((distributor) => {
               return (
-                <tr key={i} className="text-left">
-                  <td>{ i + 1 }</td>
-                  <td style={{'minWidth':"105px"}}>{ distributor.typeIdentity }-{distributor.tin}</td>
-                  <td>{ distributor.name }</td>
-                  <td>{ distributor.email[0] }</td>
-                  <td>{ distributor.phone[0] }</td>
-                  <td style={{'minWidth':"205px"}}>
+                <tr key={distributor.number + 1} className="text-left">
+                  <td>{distributor.number}</td>
+                  <td style={{ 'minWidth': "105px" }}>{distributor.typeIdentity}-{distributor.tin}</td>
+                  <td>{distributor.name}</td>
+                  <td>{distributor.email[0]}</td>
+                  <td>{distributor.phone[0]}</td>
+                  <td style={{ 'minWidth': "205px" }}>
                     <div className="float-left" >
-                      <IconButton aria-label="Delete" title="Ver Rol" className="iconButtons" onClick={() => { this.openModal(2, i, distributor.id); }}><Visibility className="iconTable" /></IconButton>
-                      <IconButton aria-label="Delete" title="Editar Rol" disabled={updateDisabled} className="iconButtons" onClick={() => { this.openModal(3, i, distributor.id); }}><Edit className="iconTable" /></IconButton>                        
-                      <IconButton aria-label="Delete" title="Editar Rol"  disabled={deleteDisabled} className="iconButtons" onClick={() => { this.deleteProveedor(distributor.id); }}><Delete className="iconTable" /></IconButton>                        
+                      <IconButton aria-label="Delete"
+                        title="Ver Rol" className="iconButtons"
+                        onClick={() => { this.openModal(2, distributor.number, distributor.id); }}>
+                        <Visibility className="iconTable" />
+                      </IconButton>
+
+                      <IconButton aria-label="Delete"
+                        title="Editar Rol" disabled={updateDisabled}
+                        className="iconButtons" onClick={() => { this.openModal(3, distributor.number, distributor.id); }}>
+                        <Edit className="iconTable" />
+                      </IconButton>
+
+                      <IconButton aria-label="Delete"
+                        title="Editar Rol" disabled={deleteDisabled}
+                        className="iconButtons" onClick={() => { this.deleteProveedor(distributor.id); }}
+                      ><Delete className="iconTable" />
+                      </IconButton>
                     </div>
-                  </td>                    
+                  </td>
                 </tr>
               );
-             })
+            })
               :
-                null
-              }
-            </tbody>
-          </Table>
-          
+              null
+            }
+          </tbody>
+        </Table>
+        <div style={{'display': "flex", 'justify-content': "flex-end"}}>
+          <Pagination contador={this.props.listDistributor}
+            page={page}
+            rowsPerPage={rowsPerPage}
+            handleChangeRowsPerPage={this.handleChangeRowsPerPage}
+            handleChangePage={this.handleChangePage} />
+        </div>
       </div>
     );
   }
