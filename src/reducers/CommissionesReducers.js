601a7f1d4737d5139184d367563993093a30b9b1
wilfredo <wilfredomedina11@gmail.com>
2019-09-10 10:10:40 -0400

    comisiones 10-09-2013

--- src/reducers/CommissionesReducers.js
+++ src/reducers/CommissionesReducers.js
@@ -1,39 +1,48 @@
 import { Map } from 'immutable'
-const setData = (state, node , payload)=> state.set(node, payload)
+import { getArray } from '../core/utils';
+const setData = (state, node, payload) => state.set(node, payload)
 
 const LoadConfigCommissionIdFunction = (state, payload) => {
 	let estado = state.toJS();
-	estado.dataId = payload.data;			
-	estado.servicesCommission = payload.data.services_commissions;			
-	estado.servicesPayment = payload.data.services_payments;			
-	estado.dataPatientsStaff = payload.data.data_options;			
+	estado.dataId = payload.data;
+	estado.servicesCommission = payload.data.services_commissions;
+	estado.servicesAllCommission = payload.data.services_commissions;
+	estado.servicesPayment = payload.data.services_payments;
+	estado.servicesAllPayment = payload.data.services_payments;
+	estado.dataPatientsStaff = payload.data.data_options;
 	return Map(estado);
 }
 
 const setActionProps = (state, payload) => {
 	let estado = state.toJS();
-	estado.action = payload;		
+	estado.action = payload;
 	return Map(estado);
 }
 
 const setPorcentajeTableComisiones = (state, payload) => {
-	let estado = state.toJS();	
-	const key = estado.servicesPayment.findIndex(service => service.serviceId === payload.id);		
-	estado.servicesPayment[key].percentage = payload.value;	
-	estado.action = 1;	
+	let estado = state.toJS();
+	const key = estado.servicesPayment.findIndex(service => service.serviceId === payload.id);
+	const key_ = estado.servicesAllPayment.findIndex(service => service.serviceId === payload.id);
+	estado.servicesPayment[key].percentage = payload.value;
+	estado.servicesAllPayment[key_].percentage = payload.value;
+	estado.action = 1;
 	return Map(estado);
 }
 
 const setSwitchTableComisiones = (state, payload) => {
 	let estado = state.toJS();
-	if(payload.tab === "2" && payload.typePersonal !== "5d1776e3b0d4a50b23936710"){
-		const key = estado.servicesCommission.findIndex(service => service.serviceId === payload.id);		
-		estado.servicesCommission[key].confirm = payload.value;		
-	}else if(payload.tab === "3"){
-		const key = estado.servicesPayment.findIndex(service => service.serviceId === payload.id);		
-		estado.servicesPayment[key].confirm = payload.value;		
+	if (payload.tab === "2" && payload.typePersonal !== "5d1776e3b0d4a50b23936710") {
+		const key = estado.servicesCommission.findIndex(service => service.serviceId === payload.id);
+		const key_ = estado.servicesAllCommission.findIndex(service => service.serviceId === payload.id);
+		estado.servicesCommission[key].confirm = payload.value;
+		estado.servicesAllCommission[key_].confirm = payload.value;
+	} else if (payload.tab === "3") {
+		const key = estado.servicesPayment.findIndex(service => service.serviceId === payload.id);
+		const key_ = estado.servicesAllPayment.findIndex(service => service.serviceId === payload.id);
+		estado.servicesPayment[key].confirm = payload.value;
+		estado.servicesAllPayment[key_].confirm = payload.value;
 	}
-	estado.action = 1;	
+	estado.action = 1;
 	return Map(estado);
 }
 const setCleanListServices = (state, payload) => {
@@ -42,104 +51,129 @@ const setCleanListServices = (state, payload) => {
 	estado.servicesCommission.map((service, i) => {
 		service.percentage = payload.percentaje;
 		service.confirm = payload.confirm;
-	})	
+	})
+	estado.servicesAllCommission.map((service, i) => {
+		service.percentage = payload.percentaje;
+		service.confirm = payload.confirm;
+	})
 	estado.servicesPayment.map((service, i) => {
 		service.percentage = payload.percentaje;
 		service.confirm = payload.confirm;
-	})	
-	
+	})
+	estado.servicesAllPayment.map((service, i) => {
+		service.percentage = payload.percentaje;
+		service.confirm = payload.confirm;
+	})
+
 	return Map(estado);
 }
 
 const setCleanListServicesTab = (state, payload) => {
 	let estado = state.toJS();
 	//estado.action = 0;
-	if(payload.tab === "2"){
+	if (payload.tab === "2") {
 		estado.servicesCommission.map((service, i) => {
 			service.percentage = payload.percentaje;
 			service.confirm = payload.confirm;
 		})
-	}else{
+		estado.servicesAllCommission.map((service, i) => {
+			service.percentage = payload.percentaje;
+			service.confirm = payload.confirm;
+		})
+	} else {
 		estado.servicesPayment.map((service, i) => {
 			service.percentage = payload.percentaje;
 			service.confirm = payload.confirm;
-		})		
+		})
+		estado.servicesAllPayment.map((service, i) => {
+			service.percentage = payload.percentaje;
+			service.confirm = payload.confirm;
+		})
 	}
 	return Map(estado);
 }
 
 const setSwitchAllTableComisiones = (state, payload) => {
 	let estado = state.toJS();
-	if(payload.tab === "2" && payload.typePersonal !== "5d1776e3b0d4a50b23936710"){		
+	if (payload.tab === "2" && payload.typePersonal !== "5d1776e3b0d4a50b23936710") {
 		estado.servicesCommission.map((commission, i) => {
 			commission.confirm = payload.value;
 			estado.action = 1;
-		})	
-	}else if(payload.tab === "3"){
+		})
+		estado.servicesAllCommission.map((commission, i) => {
+			commission.confirm = payload.value;
+			estado.action = 1;
+		})
+	} else if (payload.tab === "3") {
 		estado.servicesPayment.map((payment, i) => {
 			payment.confirm = payload.value;
 			estado.action = 1;
-		})	
+		})
+		estado.servicesAllPayment.map((payment, i) => {
+			payment.confirm = payload.value;
+			estado.action = 1;
+		})
 	}
-	
+
 	return Map(estado);
 }
 
 const setPorcentajeAllTable = (state, payload) => {
-	let estado = state.toJS();	
+	let estado = state.toJS();
 	estado.servicesPayment.map((payment, i) => {
 		payment.percentage = payload.value;
 		estado.action = 1;
-	})	
-	
+	})
+	estado.servicesAllPayment.map((payment, i) => {
+		payment.percentage = payload.value;
+		estado.action = 1;
+	})
+
 	return Map(estado);
 }
 
 const searchPatientStaffAll = (state, payload) => {
 	let estado = state.toJS();
-	estado.dataStaffPatientAll = payload;		
+	estado.dataStaffPatientAll = payload;
 	return Map(estado);
 }
 
 const searchOnePatientStaff = (state, payload) => {
-	console.log("payloaddd", payload)
 	let estado = state.toJS();
 	let obj = {
 		field_1: "patient",
-		field_2: payload.type_identity+"-"+payload.dni,
-		field_3: payload.names+" "+payload.surnames,
+		field_2: payload.type_identity + "-" + payload.dni,
+		field_3: payload.names + " " + payload.surnames,
 		field_4: payload._id,
 	}
-	estado.dataPatientsStaff.push(obj);	
+	estado.dataPatientsStaff.push(obj);
 	return Map(estado);
 }
 
 const searchOneInternalStaff = (state, payload) => {
 	let estado = state.toJS();
-	estado.dataInternalStaffAll = payload;		
+	estado.dataInternalStaffAll = payload;
 	return Map(estado);
 }
 const searchOneExternalStaff = (state, payload) => {
 	let estado = state.toJS();
-	estado.dataExternalStaffAll = payload;		
+	estado.dataExternalStaffAll = payload;
 	return Map(estado);
 }
 
 const getOneReferenceInternal = (state, payload) => {
-	console.log("payloaddd", payload)
 	let estado = state.toJS();
 	let obj = {
 		field_1: "internal_staff",
 		field_2: payload.branchoffices,
-		field_3: payload.names+" "+payload.surnames,
+		field_3: payload.names + " " + payload.surnames,
 		field_4: payload._id
 	}
-	estado.dataPatientsStaff.push(obj);	
+	estado.dataPatientsStaff.push(obj);
 	return Map(estado);
 }
 
-const getOneReferenceExternal = (state, payload) => {	
-	console.log("payloaddd", payload)
+const getOneReferenceExternal = (state, payload) => {
 	let estado = state.toJS();
 	let obj = {
 		field_1: "external_staff",
@@ -147,85 +181,104 @@ const getOneReferenceExternal = (state, payload) => {
 		field_3: payload.branchoffices,
 		field_4: payload.branchoffices_id
 	}
-	estado.dataPatientsStaff.push(obj);	
+	estado.dataPatientsStaff.push(obj);
 	return Map(estado);
 }
 
 const removerRegisterFunction = (state, payload) => {
 	let estado = state.toJS();
 	var listDataPatientsStaff = estado.dataPatientsStaff;
-	listDataPatientsStaff.splice(payload.key, 1);        
-	estado.dataPatientsStaff = listDataPatientsStaff;		
-	estado.action = 1;	
+	listDataPatientsStaff.splice(payload.key, 1);
+	estado.dataPatientsStaff = listDataPatientsStaff;
+	estado.action = 1;
 	return Map(estado);
 }
 
 const cleanDataPatientsStaffs = (state, payload) => {
 	let estado = state.toJS();
-	estado.dataStaffPatientAll = payload.data;			
-	estado.dataInternalStaffAll = payload.data;			
-	estado.dataExternalStaffAll = payload.data;			
-	estado.dataPatientsStaff = payload.data;				
+	estado.dataStaffPatientAll = payload.data;
+	estado.dataInternalStaffAll = payload.data;
+	estado.dataExternalStaffAll = payload.data;
+	estado.dataPatientsStaff = payload.data;
+	return Map(estado);
+}
+
+const filterServicesAction = (state, payload) => {
+	let estado = state.toJS();
+	if (payload.tab === "2") {
+		const dataService = getArray(estado.servicesAllCommission)
+		let expresion = new RegExp(`${payload.value}.*`, "i");
+		const result = dataService.filter(data => expresion.test(data.serviceName));		
+		estado.servicesCommission = result;
+	} else {
+		const dataService = getArray(estado.servicesAllPayment)
+		let expresion = new RegExp(`${payload.value}.*`, "i");
+		const result = dataService.filter(data => expresion.test(data.serviceName));
+		estado.servicesPayment = result;
+	}
 	return Map(estado);
 }
 
 const ConfigCommissionsReducer = (state = Map(), action) => {
-  switch (action.type) {
+	switch (action.type) {
 
-	case 'LOAD_CONFIG_COMMISSIONS': 
-	  	return Map(action.payload)	  
+		case 'LOAD_CONFIG_COMMISSIONS':
+			return Map(action.payload)
 
-  	case 'LOAD_CONFIG_COMMISSION_ID': 
-	  	return LoadConfigCommissionIdFunction(state, action.payload)	
+		case 'LOAD_CONFIG_COMMISSION_ID':
+			return LoadConfigCommissionIdFunction(state, action.payload)
 
-	case 'ACTION_PROPS': 
-	  	return setActionProps(state, action.payload)	
+		case 'ACTION_PROPS':
+			return setActionProps(state, action.payload)
 
-  	case 'CLEAN_LIST_SERVICES': 
-	  	return setCleanListServices(state, action.payload)	
+		case 'CLEAN_LIST_SERVICES':
+			return setCleanListServices(state, action.payload)
 
-  	case 'CLEAN_LIST_SERVICES_TAB': 
-	  	return setCleanListServicesTab(state, action.payload)	
+		case 'CLEAN_LIST_SERVICES_TAB':
+			return setCleanListServicesTab(state, action.payload)
 
-	case "SET_PORCENTAJE_COMISIONES":
-  		return setPorcentajeTableComisiones(state, action.payload);				
+		case "SET_PORCENTAJE_COMISIONES":
+			return setPorcentajeTableComisiones(state, action.payload);
 
-	case "SET_SWITCH_COMISIONES":
-  		return setSwitchTableComisiones(state, action.payload);			
+		case "SET_SWITCH_COMISIONES":
+			return setSwitchTableComisiones(state, action.payload);
 
-  	case "SET_SWITCH_ALL_COMISIONES":
-  		return setSwitchAllTableComisiones(state, action.payload);		  
+		case "SET_SWITCH_ALL_COMISIONES":
+			return setSwitchAllTableComisiones(state, action.payload);
 
-	case "SET_PORCENTAJE_ALL_COMISIONES":
-  		return setPorcentajeAllTable(state, action.payload);		  
-	
-	case "SEARCH_PATIENTS_STAFF_ALL":
-		return searchPatientStaffAll(state, action.payload);	
-		
-	case "SEARCH_ONE_PATIENT_STAFF": 
-		return searchOnePatientStaff(state, action.payload);	
-		
-	case "OPTIONS_INTERNALS":
-		return searchOneInternalStaff(state, action.payload);
-	
-	case "OPTIONS_EXTERNAL":
-		return searchOneExternalStaff(state, action.payload);
-	
-	case "SEARCH_STAFF_INTERNAL_ONE":
-		return getOneReferenceInternal(state, action.payload);
-	
-	case "SEARCH_STAFF_EXTERNAL_ONE":
-		return getOneReferenceExternal(state, action.payload);
-	
-	case "REMOVE_REGISTER":
-		return removerRegisterFunction(state, action.payload);
-	
-	case "CLEAN_DATA_PATIENTS_STAFFS":
-		return cleanDataPatientsStaffs(state, action.payload);
+		case "SET_PORCENTAJE_ALL_COMISIONES":
+			return setPorcentajeAllTable(state, action.payload);
 
-	default:
-	  	return state;
-  }
+		case "SEARCH_PATIENTS_STAFF_ALL":
+			return searchPatientStaffAll(state, action.payload);
+
+		case "SEARCH_ONE_PATIENT_STAFF":
+			return searchOnePatientStaff(state, action.payload);
+
+		case "OPTIONS_INTERNALS":
+			return searchOneInternalStaff(state, action.payload);
+
+		case "OPTIONS_EXTERNAL":
+			return searchOneExternalStaff(state, action.payload);
+
+		case "SEARCH_STAFF_INTERNAL_ONE":
+			return getOneReferenceInternal(state, action.payload);
+
+		case "SEARCH_STAFF_EXTERNAL_ONE":
+			return getOneReferenceExternal(state, action.payload);
+
+		case "REMOVE_REGISTER":
+			return removerRegisterFunction(state, action.payload);
+
+		case "CLEAN_DATA_PATIENTS_STAFFS":
+			return cleanDataPatientsStaffs(state, action.payload);
+
+		case "SET_FILTER_SERVICES":
+			return filterServicesAction(state, action.payload);
+
+		default:
+			return state;
+	}
 };
 
 export default ConfigCommissionsReducer;
--- src/views/Comisiones/ListServices.js
+++ src/views/Comisiones/ListServices.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Table, Button, FormGroup, Input, Label } from "reactstrap";
-import IconButton from "@material-ui/core/IconButton";
-import { Delete, Edit, Visibility } from "@material-ui/icons";
+import { Table, FormGroup, Input, Label } from "reactstrap";
+
 import Switch from '@material-ui/core/Switch';
 import "./Commissions.css";
 import Pagination from '../../components/Pagination';
-import { getArray, GetDisabledPermits } from '../../core/utils';
+import { InitalState } from "./InitialState.js";
+import { getArray } from '../../core/utils';
 
 class ListServices extends React.Component {
   constructor(props) {
@@ -21,25 +21,22 @@ class ListServices extends React.Component {
       position: 0,
       id: '',
       sucursal_id_now: '',
-      searchService:'',
+      searchService: '',
       page: 0,
       rowsPerPage: 10,
-      arrayTest:getArray(this.props.data)      
+      arrayTest: getArray(this.props.data),
+      ...InitalState
     };
   }
 
   componentDidMount() { }
 
   handleChange = e => {
-    const { name, value } = e.target;    
-    const dataService = getArray(this.props.data)
-    let expresion = new RegExp(`${value}.*`, "i");
-    const result = dataService.filter(data => expresion.test(data.serviceName));    
+    const { name, value } = e.target;
+    this.props.filterServicesAction(value, this.props.tab);
     this.setState({
-      [name]: value,
-      arrayTest: result
+      [name]: value
     });
-
   };
 
   handleChangeRowsPerPage = event => {
@@ -48,154 +45,212 @@ class ListServices extends React.Component {
 
   handleChangePage = (event, page) => {
     this.setState({ page });
-  };  
+  };
 
   handleChangeInputTable = (id) => e => {
     const { name, value } = e.target;
     /*let valor = 0;
-    valor = parseFloat(value);*/    
-    this.props.setPorcentajeTable(id, value);    
+    valor = parseFloat(value);*/
+    this.props.setPorcentajeTable(id, value);
     this.props.seteardivSeleccioneServiciosComision();
   }
 
-  handleChangeSwitch = (id) => event => {    
-    this.props.setSwitchTableComisiones(id, event.target.checked, this.props.tab, this.props.typePersonal);     
-    this.props.seteardivSeleccioneServiciosComision();   
+  handleChangeSwitch = (id) => event => {
+    this.props.setSwitchTableComisiones(id, event.target.checked, this.props.tab, this.props.typePersonal);
+    this.props.seteardivSeleccioneServiciosComision();
   };
 
-  handleChangeSwitchAll = name => event => {        
-      this.setState({ 
-          [name]: event.target.checked 
-      });
-      this.props.setSwitchAllTableComisiones(event.target.checked, this.props.tab, this.props.typePersonal);      
-      this.props.seteardivSeleccioneServiciosComision();
+  handleChangeSwitchAll = name => event => {
+    this.setState({
+      [name]: event.target.checked
+    });
+    this.props.setSwitchAllTableComisiones(event.target.checked, this.props.tab, this.props.typePersonal);
+    this.props.seteardivSeleccioneServiciosComision();
   };
 
   componentWillReceiveProps = props => {
     this.setState({
-      arrayTest: getArray(props.data)      
+      arrayTest: getArray(props.data)
     })
   };
 
   eventoBlurAplicarTodos = (id) => e => {
-    if(document.getElementById("inputQuantity_"+id).value === '' || document.getElementById("inputQuantity_"+id).value === '0'){
-        document.getElementById("inputQuantity_"+id).value = '0';
-    }        
+    if (document.getElementById("inputQuantity_" + id).value === '' || document.getElementById("inputQuantity_" + id).value === '0') {
+      document.getElementById("inputQuantity_" + id).value = '0';
+    }
   }
 
-  eventoFocusAplicarTodos = (id) => e => {        
-    if(document.getElementById("inputQuantity_"+id).value === '0'){
-        document.getElementById("inputQuantity_"+id).value = '';
-    }        
-  }  
+  eventoFocusAplicarTodos = (id) => e => {
+    if (document.getElementById("inputQuantity_" + id).value === '0') {
+      document.getElementById("inputQuantity_" + id).value = '';
+    }
+  }
+
+  handlekeyAplicarPorcentajeTodos = event => {
+    this.setState({
+      divAplicarPorcentajeTodos: "",
+      divAplicarPorcentajeTodosError: "",
+    })
+  }
+
+  handleChangeInputTable_ = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+    this.props.setPorcentajeAllTable(value);
+    this.setState({
+      divSeleccioneServiciosPayment: '',
+    });
+  };
+
+  eventoBlurAplicarTodos_ = (e) => {
+    if (this.state.aplicarPorcentajeTodos === '' || this.state.aplicarPorcentajeTodos === '0') {
+      this.setState({
+        aplicarPorcentajeTodos: '0'
+      });
+    }
+  }
+
+  eventoFocusAplicarTodos_ = (e) => {
+    if (this.state.aplicarPorcentajeTodos === '0') {
+      this.setState({
+        aplicarPorcentajeTodos: ''
+      });
+    }
+  }
 
   render() {
     const { rowsPerPage, page } = this.state;
     const ArrayData = this.state.arrayTest
     return (
       <span>
-      <div align="center">
-        <FormGroup className={`top form-group col-sm-6`}>                                                                 
-          <Input 
-            disabled={this.props.disabled} 
-            name="searchService" 
-            id="searchService" 
-            onKeyUp={this.handlekeySearchService} 
-            onChange={this.handleChange} 
-            value={this.state.searchService} 
-            type="text"             
-            placeholder="Buscar Servicio..." 
-            style={{ borderRadius: "1.25rem" }}
-          />
-        </FormGroup>  
-      </div>
-      <div>         
-        <div className="errorSelect" style={{width: "100%"}}>{this.props.divSeleccioneServiciosComision}</div>
-        <div className="errorSelect" style={{width: "100%"}}>{this.props.divSeleccioneServiciosPayment}</div>
-        {
-          this.props.typePersonal !== "5d1776e3b0d4a50b23936710" &&
-          <div align="right">      
-            <Label for="seleccionarTodos"><b>Seleccionar Todos:</b></Label>
-            <Switch
-              checked={this.state.checked?this.state.checked:false}
-              onChange={this.handleChangeSwitchAll("checked")}
-              value={this.state.checked}
-              color="primary"
-              disabled={this.props.disabled} 
-            />        
-          </div>     
-        }
-        <Table hover responsive borderless>
-          <thead className="thead-light">
-            <tr>
-              <th className="text-left">Nro</th>
-              <th className="text-left">Servicio</th>
-              <th className="text-left">Categoria</th>
-              {
-                this.props.typePersonal === "5d1776e3b0d4a50b23936710" ?
-                <th className="text-left">% Ganancia</th>
-                :
-                <th className="text-left">Seleccionar</th>
-              }
-            </tr>
-          </thead>
-          <tbody>
-            {ArrayData ? ArrayData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((data, i) => {
-              return (
-                <tr key={data.number} className="text-left">
-                  <td>{data.number}</td>
-                  <td>{data.serviceName}</td>
-                  <td>{data.category}</td>
-                  {
-                    this.props.typePersonal === "5d1776e3b0d4a50b23936710" ?
-                    <td style={{padding: "9px"}}>
-                      <div id={`divQuantity_${data.serviceId}`} className="">
-                        <Input 
-                          name={`inputQuantity_${data.serviceId}`}
-                          id={`inputQuantity_${data.serviceId}`}
-                          type="number"                                                                    
-                          value={data.percentage}
-                          onChange={this.handleChangeInputTable(data.serviceId)}
-                          style={{width: "40%"}}
-                          disabled={this.props.disabled} 
-                          onBlur ={this.eventoBlurAplicarTodos(data.serviceId)} 
-                          onFocus = {this.eventoFocusAplicarTodos(data.serviceId)} 
-                        />
-                      </div>
-                    </td>
-                    :
-                    <td style={{padding: "1px"}}>
-                      <Switch
-                          onChange={this.handleChangeSwitch(data.serviceId)} 
-                          value= {data.confirm}                                             
-                          id= {`checked_${i}`}
-                          name= {`checked_${i}`}
-                          color="primary"
-                          checked={data.confirm}
-                          disabled={this.props.disabled} 
-                      />
-                    </td>      
-                  }
-                  
-                </tr>
-              );              
-            })            
-              :
-              null
-            } 
-          </tbody>    
+        <div className="row">
+          <FormGroup className={`top form-group col-sm-6`}>
+            <Input
+              disabled={this.props.disabled}
+              name="searchService"
+              id="searchService"
+              onKeyUp={this.handlekeySearchService}
+              onChange={this.handleChange}
+              value={this.state.searchService}
+              type="text"
+              placeholder="Buscar Servicio..."
+              style={{ borderRadius: "0.25rem", border: "1px solid #e4e7ea" }}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <div className="errorSelect" style={{ width: "100%" }}>{this.props.divSeleccioneServiciosComision}</div>
+          <div className="errorSelect" style={{ width: "100%" }}>{this.props.divSeleccioneServiciosPayment}</div>
           {
+            this.props.typePersonal === "5d1776e3b0d4a50b23936710" &&
+            <div className="row" style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <FormGroup className={`top form-group col-sm-6`}>
+                <div className={this.state.divAplicarPorcentajeTodos}>
+                  <Label for="aplicarPorcentajeTodos">Aplicar Porcentaje:</Label>
+                  <Input
+                    disabled={this.props.disabled}
+                    name="aplicarPorcentajeTodos"
+                    id="aplicarPorcentajeTodos"
+                    onKeyUp={this.handlekeyAplicarPorcentajeTodos}
+                    onChange={this.handleChangeInputTable_}
+                    value={this.state.aplicarPorcentajeTodos}
+                    type="number"
+                    placeholder="Aplicar Porcentaje para Todos"
+                    onBlur={this.eventoBlurAplicarTodos_}
+                    onFocus={this.eventoFocusAplicarTodos_}
+                  />
+                </div>
+                <div className="errorSelect">{this.state.divAplicarPorcentajeTodosError}</div>
+              </FormGroup>
+            </div>
+          }
+          <Table hover responsive borderless>
+            <thead className="thead-light">
+              <tr>
+                <th className="text-left">Nro</th>
+                <th className="text-left">Servicio</th>
+                <th className="text-left">Categoria</th>
+                {
+                  this.props.typePersonal === "5d1776e3b0d4a50b23936710" ?
+                    <th className="text-left">% Ganancia</th>
+                    :
+                    <th className="text-left">Seleccionar</th>
+                }
+              </tr>
+            </thead>
+            <tbody>
+              {ArrayData ? ArrayData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((data, i) => {
+                return (
+                  <tr key={data.number} className="text-left">
+                    <td>{data.number}</td>
+                    <td>{data.serviceName}</td>
+                    <td>{data.category}</td>
+                    {
+                      this.props.typePersonal === "5d1776e3b0d4a50b23936710" ?
+                        <td style={{ padding: "9px" }}>
+                          <div id={`divQuantity_${data.serviceId}`} className="">
+                            <Input
+                              name={`inputQuantity_${data.serviceId}`}
+                              id={`inputQuantity_${data.serviceId}`}
+                              type="number"
+                              value={data.percentage}
+                              onChange={this.handleChangeInputTable(data.serviceId)}
+                              style={{ width: "40%" }}
+                              disabled={this.props.disabled}
+                              onBlur={this.eventoBlurAplicarTodos(data.serviceId)}
+                              onFocus={this.eventoFocusAplicarTodos(data.serviceId)}
+                            />
+                          </div>
+                        </td>
+                        :
+                        <td style={{ padding: "1px" }}>
+                          <Switch
+                            onChange={this.handleChangeSwitch(data.serviceId)}
+                            value={data.confirm}
+                            id={`checked_${i}`}
+                            name={`checked_${i}`}
+                            color="primary"
+                            checked={data.confirm}
+                            disabled={this.props.disabled}
+                          />
+                        </td>
+                    }
 
-            this.props.data.lenght > 10 &&
+                  </tr>
+                );
+              })
+                :
+                null
+              }
+            </tbody>
+            {
+              this.props.data.lenght > 10 &&
               <Pagination contador={this.props.data}
                 page={page}
                 rowsPerPage={rowsPerPage}
                 handleChangeRowsPerPage={this.handleChangeRowsPerPage}
                 handleChangePage={this.handleChangePage} />
+            }
+          </Table>
+          {
+            this.props.typePersonal !== "5d1776e3b0d4a50b23936710" &&
+            <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <FormGroup className={`top form-group col-sm-12`}>
+                <Label for="seleccionarTodos"><b>Seleccionar Todos:</b></Label>
+                <Switch
+                  checked={this.state.checked ? this.state.checked : false}
+                  onChange={this.handleChangeSwitchAll("checked")}
+                  value={this.state.checked}
+                  color="primary"
+                  disabled={this.props.disabled}
+                />
+              </FormGroup>
+            </div>
           }
-        </Table>        
-      </div>  
-      </span>    
+        </div>
+      </span>
     );
   }
 }
