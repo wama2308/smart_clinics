f6fb368e46648b11b317a736193a5a4f60f6398f
wilfredo <wilfredomedina11@gmail.com>
2019-10-05 12:00:32 -0400

    05-10-2019

--- src/core/connection.js
+++ src/core/connection.js
@@ -1,12 +1,12 @@
-export const url = `http://35.198.5.148/sc-admin/web/app.php`;
-//export const url = `http://192.168.1.121:8000`;
+//export const url = `http://35.198.5.148/sc-admin/web/app.php`;
+export const url = `http://192.168.1.121:8000`;
 
 export const getDataToken = () => {
-  return new Promise(resolve => {
-    const token = window.localStorage.getItem("id_token");
-    const datos = {
-      headers: { "access-token": token }
-    };
-    resolve(datos);
-  });
+    return new Promise(resolve => {
+        const token = window.localStorage.getItem("id_token");
+        const datos = {
+            headers: { "access-token": token }
+        };
+        resolve(datos);
+    });
 };
